dfe9ca10c2e51ae3bc4a85a9f868b78d9119e08a
rai <96561881+r4ai@users.noreply.github.com>
2025-05-15 00:44:19 +0900

    chore: fix biome check errors manually

--- app/islands/counter.tsx
+++ app/islands/counter.tsx
@@ -6,6 +6,7 @@ export default function Counter() {
     <div>
       <p class="py-2 text-2xl">{count}</p>
       <button
+        type="button"
         class="px-4 py-2 bg-orange-400 text-white rounded cursor-pointer"
         onClick={() => setCount(count + 1)}
       >
