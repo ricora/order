bf24abecd97520077f0d1039413a458dd043a832
Kattyan <hello@kattyan.dev>
2025-05-15 00:16:57 +0900

    chore: change .prettierrc.json -> prettier.config.js
    Co-authored-by: Rai <96561881+r4ai@users.noreply.github.com>

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  semi: false,
+}
+
+export default config
