a17463753063fd07631643f00c08e8d0fc7c7b3b
KATO So <hello@kattyan.dev>
2025-05-15 17:41:53 +0900

    chore: setup prettier (#25)
    Co-authored-by: Rai <96561881+r4ai@users.noreply.github.com>

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  semi: false,
+  plugins: ["prettier-plugin-toml"],
+}
+
+export default config
