212816749462a841a0dbd6297748e061cc3331ec
rai <96561881+r4ai@users.noreply.github.com>
2025-05-29 13:41:00 +0900

    chore: use react instead of hono/jsx

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,5 +1,12 @@
 import type {} from "hono"
+import "@hono/react-renderer"
 
 declare module "hono" {
   interface Env {}
+}
+
+declare module "@hono/react-renderer" {
+  interface Props {
+    title?: string
+  }
 }
