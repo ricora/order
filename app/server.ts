670a3e202a4c5665b746a9f5a3cb3878cb5dd834
rai <96561881+r4ai@users.noreply.github.com>
2025-06-13 13:20:30 +0900

    refactor: upgrade honox and remove unnecessary config

--- app/server.ts
+++ app/server.ts
@@ -1,21 +1,7 @@
 import { showRoutes } from "hono/dev"
 import { createApp } from "honox/server"
 
-const ROUTES = import.meta.glob(
-  [
-    "/app/routes/**/!(_*|-*|$*|*.test|*.spec).(ts|tsx|md|mdx)",
-    "/app/routes/.well-known/**/!(_*|-*|$*|*.test|*.spec).(ts|tsx|md|mdx)",
-    "!/app/routes/**/-*/**/*",
-  ],
-  {
-    eager: true,
-  },
-)
-
-const app = createApp({
-  // @ts-expect-error
-  ROUTES,
-})
+const app = createApp()
 
 showRoutes(app)
 
