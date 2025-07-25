2999a8529661daebbee7006559bd5a690ccbeadc
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-07-25 11:13:41 +0900

    chore: remove react dependency

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,12 +1,5 @@
 import type {} from "hono"
-import "@hono/react-renderer"
 
 declare module "hono" {
   interface Env {}
-}
-
-declare module "@hono/react-renderer" {
-  interface Props {
-    title?: string
-  }
 }
--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,18 +1,17 @@
-import { reactRenderer } from "@hono/react-renderer"
+import { jsxRenderer } from "hono/jsx-renderer"
 import { Link, Script } from "honox/server"
 
-export default reactRenderer(({ children, title }) => {
+export default jsxRenderer(({ children }) => {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <Link href="/app/style.css" rel="stylesheet" />
         <Script src="/app/client.ts" async />
-        {title && <title>{title}</title>}
       </head>
-      <body className="">{children}</body>
+      <body>{children}</body>
     </html>
   )
 })
