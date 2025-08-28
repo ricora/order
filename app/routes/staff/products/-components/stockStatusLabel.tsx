8b66555e45e9ede23d07602e04c69a53d18ac6e4
Rai <96561881+r4ai@users.noreply.github.com>
2025-08-29 00:21:04 +0900

    chore: add tailwind-variants (#98)
    Co-authored-by: 3w36zj6 <52315048+3w36zj6@users.noreply.github.com>

---
+++ app/routes/staff/products/-components/stockStatusLabel.tsx
@@ -0,0 +1,56 @@
+import { tv } from "tailwind-variants"
+import TriangleAlertIcon from "../../../../components/icons/lucide/triangleAlertIcon"
+
+const getStockStatus = (stock: number) => {
+  if (stock === 0) {
+    return "out-of-stock"
+  }
+  if (stock <= 5) {
+    return "low-stock"
+  }
+  return "in-stock"
+}
+
+type StockStatus = ReturnType<typeof getStockStatus>
+
+const getStockStatusLabel = (status: StockStatus) => {
+  switch (status) {
+    case "out-of-stock":
+      return "在庫切れ"
+    case "low-stock":
+      return "残りわずか"
+    case "in-stock":
+      return "在庫あり"
+  }
+}
+
+const stockStatusLabel = tv({
+  slots: {
+    base: "inline-flex items-center gap-1 px-2 py-1 rounded text-xs font-semibold",
+    alertIcon: "h-3 w-3 mr-1",
+  },
+  variants: {
+    status: {
+      "out-of-stock": { base: "bg-red-100 text-red-700" },
+      "low-stock": {
+        base: "bg-yellow-100 text-yellow-700",
+        alertIcon: "hidden",
+      },
+      "in-stock": { base: "bg-gray-100 text-gray-700", alertIcon: "hidden" },
+    } satisfies Record<StockStatus, unknown>,
+  },
+})
+
+export const StockStatusLabel = ({ stock }: { stock: number }) => {
+  const status = getStockStatus(stock)
+  const { base, alertIcon } = stockStatusLabel({ status })
+
+  return (
+    <span className={base()}>
+      <div className={alertIcon()}>
+        <TriangleAlertIcon />
+      </div>
+      {getStockStatusLabel(status)}
+    </span>
+  )
+}
