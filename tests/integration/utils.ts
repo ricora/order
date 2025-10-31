8dab5b90d8fb76092597e2387eac28a6b7886e14
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-11-01 07:07:02 +0900

    test: split integration tests into separate files by route

---
+++ tests/integration/utils.ts
@@ -0,0 +1,38 @@
+import crypto from "node:crypto"
+import { createApp } from "honox/server"
+import { expect } from "vitest"
+
+export const app = createApp()
+
+export const assertBasicHtmlResponse = (res: Response, html: string) => {
+  expect(res.status).toBe(200)
+  expect(res.headers.get("content-type")).toMatch(/text\/html/)
+  expect(html).toMatch(/<!DOCTYPE html>/i)
+  expect(html).toMatch(/<html[^>]*>/i)
+  expect(html).toMatch(/<body[^>]*>/i)
+}
+
+export const generateUniqueName = (prefix: string = "") => {
+  const name = `${prefix}_${crypto.randomUUID()}`
+  const hash = crypto.createHash("sha1").update(name).digest("hex")
+  return `${prefix}_${hash}`
+}
+
+export const assertSuccessRedirect = (
+  res: Response,
+  expectedMessage: string,
+) => {
+  expect(res.status).toBe(302)
+  expect(res.headers.get("set-cookie")).toMatch(/success/)
+  expect(res.headers.get("set-cookie")).toMatch(
+    encodeURIComponent(expectedMessage),
+  )
+}
+
+export const assertErrorRedirect = (res: Response, expectedMessage: string) => {
+  expect(res.status).toBe(302)
+  expect(res.headers.get("set-cookie")).toMatch(/error/)
+  expect(res.headers.get("set-cookie")).toMatch(
+    encodeURIComponent(expectedMessage),
+  )
+}
