1b79e339948a9f068de0afb980b087eb53d5d57e
Kattyan <hello@kattyan.dev>
2025-11-21 10:58:44 +0900

    feat: add route / to /staff

---
+++ app/routes/healthz/index.tsx
@@ -0,0 +1,13 @@
+import { createRoute } from "honox/factory"
+import Counter from "../../islands/counter"
+
+export default createRoute((c) => {
+  const name = c.req.query("name") ?? "Hono"
+  return c.render(
+    <div className="py-8 text-center">
+      <title>{name}</title>
+      <h1 className="font-bold text-3xl">Hello, {name}!</h1>
+      <Counter />
+    </div>,
+  )
+})
